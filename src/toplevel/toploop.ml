1e7b04c91f04d6c407cb04f9da06b3909f975992
Pierre Talbot <ptalbot@hyc.io>
2013-12-12 23:04:29 +0100

    Add license headers.

--- src/toplevel/toploop.ml
+++ src/toplevel/toploop.ml
@@ -1,3 +1,7 @@
+(* Distributed under the MIT License.
+  (See accompanying file LICENSE.txt)
+  (C) Copyright Vincent Botbol
+*)
 
 let initialize_toplevel_env () =
   ()
--- test/parser-test.ml
+++ test/parser-test.ml
@@ -1,3 +1,8 @@
+(* Distributed under the MIT License.
+  (See accompanying file LICENSE.txt)
+  (C) Copyright Matthieu Dien
+*)
+
 let parse_chan = fun chan ->
   let lexbuf = Lexing.from_channel chan in
   let res = Parser.start Lexer.token lexbuf in
