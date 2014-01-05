a99d085ed990a2971b311083c0b2593e64f1a6f3
pcouderc <pierrickcouderc@gmail.com>
2014-01-05 17:55:36 +0100

    Added missing comment

--- src/term_system/matching.mli
+++ src/term_system/matching.mli
@@ -5,5 +5,10 @@
 
 type 'info placeholders
 
+(** [matching term pattern] returns a boolean and a map containing the possible
+  placeholders if the term matches the pattern given.
+    We suppose that the term and the pattern are well-constructed since there is
+  a typing phase.
+*)
 val matching : 'info Term_ast.expression -> 'info Ast.term_pattern
   -> bool * 'info placeholders
