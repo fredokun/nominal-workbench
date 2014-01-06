3a580685d8df3c1dc88df8ebd9a6a227dac8a4dc
Matthieu Dien <matthieu@linux-1ics.site>
2014-01-06 15:37:42 +0100

    Add escaping to deal special characters

--- src/generator/error_gen.ml
+++ src/generator/error_gen.ml
@@ -1,5 +1,6 @@
 open Printf
 open Property
+open String
 
 let make_error_types properties error_file =
   let make_error_type (name,_) =
@@ -9,13 +10,13 @@ let make_error_types properties error_file =
 
 let make_error_map_name properties error_file =
   let map_name (name,_) =
-    fprintf error_file "  | %s -> \"%s\"\n" name name in
+    fprintf error_file "  | %s -> \"%s\"\n" name (String.escaped name) in
   fprintf error_file "let string_of_error_code = function\n";
   List.iter map_name properties
 
 let make_description_map properties error_file =
   let make_description (name, desc) =
-    fprintf error_file "  | %s -> \"%s\"\n" name desc in
+    fprintf error_file "  | %s -> \"%s\"\n" name (String.escaped desc) in
   fprintf error_file "let description_of_error_code = function\n";
   List.iter make_description properties
 
