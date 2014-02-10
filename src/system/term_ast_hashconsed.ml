ad041327dbd89480498ef133345a41cccd6e0292
pcouderc <pierrickcouderc@gmail.com>
2014-02-10 11:31:03 +0100

    Draft for de Bruijn indexes

--- src/system/term_ast_hashconsed.ml
+++ src/system/term_ast_hashconsed.ml
@@ -62,10 +62,14 @@ module HListtbl = Hashtbl.Make(struct
     let hash = hash_hlist
   end)
 
+module SMap = Map.Make(String)
+
 (* Not tailrec, but an operator doesn't have thousands of subterm *)
-let rec create_hlist = function
+let rec create_hlist bindings = function
   | [] -> nil
-  | hd :: tl -> create_cons (create_term hd) (create_hlist tl)
+  | hd :: tl ->
+    let term, bindings = create_term bindings hd in
+    create_cons term (create_hlist bindings tl)
 
 and create_cons =
   let open HListtbl in
@@ -81,24 +85,30 @@ and create_cons =
       new_hl
 
 and create_term =
-  let binder = HBinder (ref [])
+  let binder = HBinder (ref []) in
   let open HTermtbl in
   let term_tbl = create 43 in
-  fun td ->
-    let term =
+  fun bindings td ->
+    let term, bindings =
       match td with
-      | DConst i -> HConst i
-      | DVar (id, Some _) -> HVar 42 (* Some computations to do for de Bruijn *)
-      | DVar (id, None) -> HFreeVar id
+      | DConst i -> HConst i, bindings
+      | DVar (id, Some _) -> HVar (SMap.find id bindings), bindings
+      (* Some computations to do for de Bruijn *)
+      | DVar (id, None) -> HFreeVar id, bindings
       | DTerm (id, l) ->
-        let hl = create_hlist l in
-        HTerm (id, hl)
+        let hl = create_hlist bindings l in
+        HTerm (id, hl), bindings
 
-(* The hashconsing of the binder is a problem for now, since we need its binded
-  variable to instantiate is *)
-      | DBinder (id, _) ->  binder(* HBinder (ref []) *) in
-    try find term_tbl term
+      (* The hashconsing of the binder is a problem for now, since we need its binded
+         variable to instantiate is *)
+      | DBinder (id, _) ->
+        let bindings = SMap.add id 0 @@
+          SMap.map (fun i -> i + 1) bindings in
+        binder, bindings in
+
+    (* Should we add the current bindings in the hashtbl ? Technically no, we
+       need some case were it is mandatory *)
+    try find term_tbl term, bindings
     with Not_found ->
       add term_tbl term term;
-      term
-(* each constructor has its own hashconsing function to get its equivalent *)
+      term, bindings
