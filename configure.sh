3a852f4958f60720b223875ece9c39a3ad5f4998
pcouderc <pierrickcouderc@gmail.com>
2014-03-19 09:44:00 +0100

    Updated opam package and configure to export the datas

--- configure.sh
+++ configure.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-opam install xml-light
+opam install xml-light ocp-build
 ocp-build -init -configure
+
+if [ $1 = "--data" ] && [ -d $2 ]; then
+    cp -r data/test $2
+    cp data/nowork.el $2
+fi
